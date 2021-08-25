106e3b05dd182e24cef70a66df24360ac3c34c8d
ilya <kornienko.i@mnogoslov.com>
2021-08-25 12:26:33 +0300

    fix redirect

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -20,13 +20,19 @@ class Bootstrap implements BootstrapInterface {
     
     public function bootstrap($app) {
         $this->redirect($app);
+        $this->addRules($app);
     }
     
     private function redirect($app) {
         $alias = PmAlias::find()->where(['url'=>$app->request->getPathInfo()])->limit(1)->one();
         if($alias) {
-            $app->response->redirect([$alias->page->path], 302)->send();
-            die();
+            $redirectUrl = PmAlias::find()->where(['page_id'=>$alias->page_id])->orderBy('id desc')->limit(1)->one();
+            
+            if($redirectUrl && $redirectUrl->id != $alias->id) {
+                $app->response->redirect([$redirectUrl->url], 301)->send();
+                die();
+            }
+            
         }
     }
     
@@ -42,12 +48,13 @@ class Bootstrap implements BootstrapInterface {
                 . " LEFT JOIN (SELECT Max(id) as max_id, page_id FROM {{%pm_alias}} GROUP BY `page_id`) as max ON max.page_id = alias.page_id"
                 . " LEFT JOIN {{%page_managment}} as pm ON pm.id = alias.page_id"
                 . " WHERE alias.id = max.max_id;")->asArray()->all();*/
-        $alias = PmAlias::find()->select('{{%pm_alias}}.*, {{%page_managment}}.path')->leftJoin('{{%page_managment}}', '{{%page_managment}}.id={{%pm_alias}}.page_id')->asArray()->all();
-        $result = false;
-        foreach ($alias as $alia) {
+        //$alias = PmAlias::find()->select('{{%pm_alias}}.*, {{%page_managment}}.path')->leftJoin('{{%page_managment}}', '{{%page_managment}}.id={{%pm_alias}}.page_id')->asArray()->all();
+        $alias = PmAlias::find()->select('{{%pm_alias}}.*, {{%page_managment}}.path')->leftJoin('{{%page_managment}}', '{{%page_managment}}.id={{%pm_alias}}.page_id')->where(['{{%pm_alias}}.url'=>$app->request->getPathInfo()])->limit(1)->asArray()->one();
+        $result = false;        
+        if($alias) {
             $result[] = [
-                'pattern'=>$alia['url'],
-                'route' => $alia['path'],
+                'pattern'=>$alias['url'],
+                'route' => $alias['path'],
                 ];
         }
         return $result;
